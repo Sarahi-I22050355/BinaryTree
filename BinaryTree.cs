3509648eff0133d7a64c25a6b67e5aaa2eda369c
Sarah <Sarah@HP-SARAHI>
2023-10-05 21:53:25 -0600

    Agregar archivos de proyecto.

---
+++ BinaryTree.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    internal class BinaryTree
+    {
+        private Node root;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        public void Insert(int value)
+        {
+            root = InsertRecursive(root, value);
+        }
+
+        private Node InsertRecursive(Node currentNode, int value)
+        {
+            if (currentNode == null)
+            {
+                return new Node(value);
+            }
+
+            if (value < currentNode.Value)
+            {
+                currentNode.Left = InsertRecursive(currentNode.Left, value);
+            }
+            else if (value > currentNode.Value)
+            {
+                currentNode.Right = InsertRecursive(currentNode.Right, value);
+            }
+
+            return currentNode;
+        }
+
+        public void Delete(int value)
+        {
+            root = DeleteRecursive(root, value);
+        }
+
+        private Node DeleteRecursive(Node currentNode, int value)
+        {
+            if (currentNode == null)
+            {
+                return currentNode;
+            }
+
+            if (value < currentNode.Value)
+            {
+                currentNode.Left = DeleteRecursive(currentNode.Left, value);
+            }
+            else if (value > currentNode.Value)
+            {
+                currentNode.Right = DeleteRecursive(currentNode.Right, value);
+            }
+            else
+            {
+                if (currentNode.Left == null)
+                {
+                    return currentNode.Right;
+                }
+                else if (currentNode.Right == null)
+                {
+                    return currentNode.Left;
+                }
+
+                currentNode.Value = FindMinValue(currentNode.Right);
+
+                currentNode.Right = DeleteRecursive(currentNode.Right, currentNode.Value);
+            }
+
+            return currentNode;
+        }
+
+        private int FindMinValue(Node currentNode)
+        {
+            int minValue = currentNode.Value;
+            while (currentNode.Left != null)
+            {
+                minValue = currentNode.Left.Value;
+                currentNode = currentNode.Left;
+            }
+            return minValue;
+        }
+
+        public bool Search(int value)
+        {
+            return SearchRecursive(root, value);
+        }
+
+        private bool SearchRecursive(Node currentNode, int value)
+        {
+            if (currentNode == null)
+            {
+                return false;
+            }
+
+            if (value == currentNode.Value)
+            {
+                return true;
+            }
+
+            if (value < currentNode.Value)
+            {
+                return SearchRecursive(currentNode.Left, value);
+            }
+
+            return SearchRecursive(currentNode.Right, value);
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+
+            Console.WriteLine("Binary Tree:");
+            while (true)
+            {
+                Console.WriteLine("1. Insert value");
+                Console.WriteLine("2. Search value");
+                Console.WriteLine("3. Delete value");
+                Console.WriteLine("4. Exit");
+
+                Console.Write("Choose an option: ");
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.Write("Enter a value to insert: ");
+                        int valueToInsert = int.Parse(Console.ReadLine());
+                        tree.Insert(valueToInsert);
+                        break;
+                    case 2:
+                        Console.Write("Enter a value to search: ");
+                        int valueToSearch = int.Parse(Console.ReadLine());
+                        bool found = tree.Search(valueToSearch);
+                        if (found)
+                        {
+                            Console.WriteLine("The value is found in the tree.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("The value is not found in the tree.");
+                        }
+                        break;
+                    case 3:
+                        Console.Write("Enter a value to delete: ");
+                        int valueToDelete = int.Parse(Console.ReadLine());
+                        tree.Delete(valueToDelete);
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Please choose a valid option.");
+                        break;
+                }
+            }
+        }
+    }
+}
